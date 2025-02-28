f978d103615b22d901853d5b5c25fd4309b27c66
Brandon <32753167+btoms20@users.noreply.github.com>
2025-02-28 12:06:41 -0800

    formatted

--- Package.swift
+++ Package.swift
@@ -25,13 +25,14 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "CID",
-            targets: ["CID"]),
+            targets: ["CID"]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/swift-libp2p/swift-multibase.git", .upToNextMajor(from: "0.0.1")),
         .package(url: "https://github.com/swift-libp2p/swift-multicodec.git", .upToNextMajor(from: "0.0.1")),
-        .package(url: "https://github.com/swift-libp2p/swift-multihash.git", .upToNextMajor(from: "0.0.1"))
+        .package(url: "https://github.com/swift-libp2p/swift-multihash.git", .upToNextMajor(from: "0.0.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -42,9 +43,11 @@ let package = Package(
                 .product(name: "Multihash", package: "swift-multihash"),
                 .product(name: "Multibase", package: "swift-multibase"),
                 .product(name: "Multicodec", package: "swift-multicodec"),
-            ]),
+            ]
+        ),
         .testTarget(
             name: "CIDTests",
-            dependencies: ["CID"]),
+            dependencies: ["CID"]
+        ),
     ]
 )
